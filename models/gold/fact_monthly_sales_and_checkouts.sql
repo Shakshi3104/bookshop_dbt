809e7f7e12812a8ad8fe087ef7e6adfe952446ca
kw-shakshi3104 <103113049+kw-shakshi3104@users.noreply.github.com>
2025-05-18 14:29:35 +0900

    [add] add fact_monthly_sales_and_checkouts

---
+++ models/gold/fact_monthly_sales_and_checkouts.sql
@@ -0,0 +1,23 @@
+with monthly_sales as (
+    select
+        book_id,
+        date_trunc(sales_date, month) as sales_month,
+        count(book_id) as amount
+
+    from {{ ref('silver_sales') }}
+    group by book_id, date_trunc(sales_date, month)
+),
+final as (
+    select
+        monthly_sales.book_id,
+        ifnull(sales_month, checkout_month) as year_month,
+        monthly_sales.amount,
+        checkouts.number_of_checkouts as checkouts
+
+    from monthly_sales
+    left join {{ ref('silver_checkouts') }} as checkouts
+    on monthly_sales.book_id = checkouts.book_id
+    and monthly_sales.sales_month = checkouts.checkout_month
+)
+
+select * from final
